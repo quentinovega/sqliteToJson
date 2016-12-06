d13bbdaa51227cca1f43b48414dd57800b83a1f6
Quentin <quentino.vega@gmail.com>
2016-12-06 19:43:32 +0100

    use json_lib to build json style database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.reactivecouchbase.json.JsObject;
+import org.reactivecouchbase.json.Json;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import static org.reactivecouchbase.json.Syntax.$;
 
 
 public class Main {
@@ -14,6 +18,20 @@ public class Main {
         try (Connection conn = connexion.connect()) {
             DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
             databaseSelectionUtil.getBook(create);
+
+            JsObject database = Json.obj(
+                    $("books", databaseSelectionUtil.getBook(create)),
+                    $("series", databaseSelectionUtil.getSeries(create)),
+                    $("artists", databaseSelectionUtil.getArtists(create)),
+                    $("authors", databaseSelectionUtil.getAuthors(create)),
+                    $("editors", databaseSelectionUtil.getEditors(create)),
+                    $("formats", databaseSelectionUtil.getFormats(create)),
+                    $("styles", databaseSelectionUtil.getStyles(create)),
+                    $("locations", databaseSelectionUtil.getLocations(create))
+            );
+
+            System.out.println(database.stringify(true));
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/databaseSelectionUtil.java
+++ src/main/java/databaseSelectionUtil.java
@@ -1,15 +1,10 @@
 import org.jooq.DSLContext;
 import org.jooq.Result;
-import org.jooq.util.maven.example.tables.Serie;
 import org.jooq.util.sqlite.SQLiteDataType;
 import org.reactivecouchbase.json.JsArray;
-import org.reactivecouchbase.json.JsObject;
 import org.reactivecouchbase.json.Json;
 
-import java.util.List;
-
 import static org.jooq.impl.DSL.one;
-import static org.jooq.impl.DSL.select;
 import static org.jooq.util.maven.example.Tables.*;
 import static org.jooq.util.maven.example.tables.Livre.LIVRE;
 import static org.jooq.util.sqlite.SQLiteDSL.rowid;
@@ -17,7 +12,7 @@ import static org.reactivecouchbase.json.Syntax.$;
 
 public class databaseSelectionUtil {
 
-    public static void getBook(DSLContext create) {
+    public static JsArray getBook(DSLContext create) {
         Result<?> result =
                 create
                         .select(
@@ -25,54 +20,47 @@ public class databaseSelectionUtil {
                                 LIVRE.TITRE,
                                 LIVRE.SOUS_TITRE,
                                 LIVRE.PRIX,
-                                LIVRE.ID_AUTEUR,
-                                LIVRE.ID_ARTISTE,
+                                LIVRE.ID_AUTEUR.cast(SQLiteDataType.BIGINT).as("id_auteur"),
+                                LIVRE.ID_ARTISTE.cast(SQLiteDataType.BIGINT).as("id_artiste"),
                                 LIVRE.ID_FORMAT,
                                 LIVRE.ID_EDITEUR,
                                 LIVRE.ID_GENRE,
                                 LIVRE.ID_LOCALISATION,
                                 LIVRE.ISBN,
-                                LIVRE.DATE_PUBLICATION,
+                                LIVRE.DATE_PUBLICATION.cast(SQLiteDataType.DATE).as("publication"),
                                 LIVRE.COMMENTAIRE,
                                 LIVRE.RESUME,
-                                LIVRE.DATE_ACHAT,
+                                LIVRE.DATE_ACHAT.cast(SQLiteDataType.DATE).as("achat"),
                                 LIVRE.RECOMPENSE
                         )
                         .from(LIVRE)
                         .orderBy(one().asc())
                         .fetch();
 
-//        JsArray books = Json.arr(
-//                result.map(record -> Json.obj(
-//                        $("id", record.getValue(one())),
-//                        $("title", record.getValue(LIVRE.TITRE)),
-//                        $("subTitle", record.getValue(LIVRE.SOUS_TITRE)),
-//                        $("price", record.getValue(LIVRE.PRIX).longValue()),
-//                        $("author", record.getValue(LIVRE.ID_AUTEUR.cast(SQLiteDataType.INT))),
-//                        $("artist", record.getValue(LIVRE.ID_ARTISTE.cast(SQLiteDataType.INT))),
-//                        $("titre", record.getValue(LIVRE.ID_FORMAT)),
-//                        $("titre", record.getValue(LIVRE.ID_EDITEUR)),
-//                        $("titre", record.getValue(LIVRE.ID_GENRE)),
-//                        $("titre", record.getValue(LIVRE.ID_LOCALISATION)),
-//                        $("titre", record.getValue(LIVRE.ISBN)),
-//                        $("titre", record.getValue(LIVRE.DATE_PUBLICATION)),
-//                        $("titre", record.getValue(LIVRE.COMMENTAIRE)),
-//                        $("titre", record.getValue(LIVRE.RESUME)),
-//                        $("titre", record.getValue(LIVRE.DATE_ACHAT)),
-//                        $("titre", record.getValue(LIVRE.RECOMPENSE))
-//                )));
-
-        result.forEach(record -> {
-            String title = record.getValue(LIVRE.TITRE);
-            String subtitle = record.getValue(LIVRE.SOUS_TITRE);
-            String isbn = record.getValue(LIVRE.ISBN);
-            System.out.println(title + " - " + subtitle + " - " + isbn);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("title", record.getValue(LIVRE.TITRE)),
+                        $("subTitle", record.getValue(LIVRE.SOUS_TITRE)),
+                        $("price", record.getValue(LIVRE.PRIX).longValue()),
+                        $("author", (Long) record.getValue("id_auteur")),
+                        $("artist", (Long) record.getValue("id_artiste")),
+                        $("format", record.getValue(LIVRE.ID_FORMAT)),
+                        $("editor", record.getValue(LIVRE.ID_EDITEUR)),
+                        $("style", record.getValue(LIVRE.ID_GENRE)),
+                        $("location", record.getValue(LIVRE.ID_LOCALISATION)),
+                        $("isbn", (String) record.getValue(LIVRE.ISBN)),
+                        $("publicationDate", record.getValue("publication").toString()),
+                        $("comment", record.getValue(LIVRE.COMMENTAIRE)),
+                        $("synopsis", record.getValue(LIVRE.RESUME)),
+                        $("buyingDate", record.getValue("achat").toString()),
+                        $("award", record.getValue(LIVRE.RECOMPENSE))
+                )));
 
 
     }
 
-    public static void getSerie(DSLContext create) {
+    public static JsArray getSeries(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), SERIE.NOM)
@@ -80,14 +68,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(SERIE.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(SERIE.NOM))
+                )));
     }
 
-    public static void getArtist(DSLContext create) {
+    public static JsArray getArtists(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), ARTISTE.NOM)
@@ -95,14 +83,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(ARTISTE.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(ARTISTE.NOM))
+                )));
     }
 
-    public static void getAuthor(DSLContext create) {
+    public static JsArray getAuthors(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), AUTEUR.NOM)
@@ -110,14 +98,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(AUTEUR.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(AUTEUR.NOM))
+                )));
     }
 
-    public static void getEditor(DSLContext create) {
+    public static JsArray getEditors(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), EDITEUR.NOM)
@@ -125,14 +113,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(EDITEUR.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(EDITEUR.NOM))
+                )));
     }
 
-    public static void getFormat(DSLContext create) {
+    public static JsArray getFormats(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), FORMAT.NOM)
@@ -140,14 +128,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(FORMAT.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(FORMAT.NOM))
+                )));
     }
 
-    public static void getStyle(DSLContext create) {
+    public static JsArray getStyles(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), GENRE.NOM)
@@ -155,14 +143,14 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(GENRE.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(GENRE.NOM))
+                )));
     }
 
-    public static void getLocation(DSLContext create) {
+    public static JsArray getLocations(DSLContext create) {
         Result<?> result =
                 create
                         .select(rowid().as("id"), LOCALISATION.NOM)
@@ -170,11 +158,11 @@ public class databaseSelectionUtil {
                         .orderBy(one().asc())
                         .fetch();
 
-        result.forEach(record -> {
-            Long id = (Long) record.getValue("id");
-            String name = record.getValue(LOCALISATION.NOM);
-            System.out.println(id + " - " + name);
-        });
+        return Json.arr(
+                result.map(record -> Json.obj(
+                        $("id", (Long) record.getValue("id")),
+                        $("name", record.getValue(LOCALISATION.NOM))
+                )));
     }
 
 
